537324ce2a7a92cb6222dfb72ed789bfd0d56d49
Theo Weller <weller.theodor@gmail.com>
2021-03-02 13:10:31 -0600

    Module 1.1 - Simple app to create Vue controlled HTML section to demonstrate interpolation

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            courseGoal: 'Learn Vue and nail the interrview for Dot Dash!'
+        };
+    }
+});
+
+app.mount('#user-goal');
