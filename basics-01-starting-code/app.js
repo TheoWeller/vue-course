318cec3890e6dd8048e1601f38202cc3a1edce96
Theo Weller <weller.theodor@gmail.com>
2021-03-02 13:17:23 -0600

    Module 1.2 - v-bind feature demonstration

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data(){
         return {
-            courseGoal: 'Learn Vue and nail the interrview for Dot Dash!'
+            courseGoal: 'Learn Vue and nail the interrview for Dot Dash!',
+            myGitHubUrl: 'https://github.com/TheoWeller'
         };
     }
 });
