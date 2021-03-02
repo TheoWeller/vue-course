1d7bf76cc8064f3b2ddd3c024167fcf20b9fa943
Theo Weller <weller.theodor@gmail.com>
2021-03-02 14:00:03 -0600

    Module 1.4 - v-html driective demo

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data(){
         return {
-            courseGoal: 'Learn Vue and nail the interrview for Dot Dash!',
+            courseGoalA: 'Learn Vue and nail the interview for Dot Dash!',
+            courseGoalB: '<h1>Refactor existing React applications to be more performant.<h1/>',
             myGitHubUrl: 'https://github.com/TheoWeller'
         };
     },
     methods:{
         outputGoal(){
             const randomNum = Math.random();
-            return randomNum < 0.5 ? "Master Vue!" : "Learn Vue";
+            return randomNum < 0.5 ? this.courseGoalA : this.courseGoalB;
         }
     }
 });
