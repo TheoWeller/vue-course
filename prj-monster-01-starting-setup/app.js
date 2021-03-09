1c7dc4edee098391650a072ef3723e8850a570de
Theo Weller <weller.theodor@gmail.com>
2021-03-08 18:35:30 -0700

    Project 1 - Added attack and special attack functions

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -1,24 +1,48 @@
 function getRandomValue(min, max){ return Math.floor(Math.random() * (max - min) + min); };
 
+// let 
+
 const app = Vue.createApp({
     data(){
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0
         };
     },
     methods: {
         attackMonster(){
+            this.currentRound++
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;         
+        },
+        specialAttackMonster(){
+            this.currentRound++
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     },
     computed: {
-
+        monsterBarStyles(){
+            return {
+                width: this.monsterHealth + '%'
+            };
+        },
+        playerBarStyles(){
+            return {
+                width: this.playerHealth + '%'
+            };
+        },
+        mayUseSpecialAttack(){
+            console.log(`${this.currentRound} ${this.currentRound % 3 !== 0}`);
+            return this.currentRound % 3 !== 0;
+        },
     },
 });
 
