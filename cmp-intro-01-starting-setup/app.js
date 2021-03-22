f450b1b5cb2973a25beb4b2cb8f106aca71858c6
Theo Weller <weller.theodor@gmail.com>
2021-03-22 14:50:46 -0700

    Components 1.1 - Created first Vue component - Encapsulated HTML w/ connected data

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: "Tom",
@@ -18,12 +17,34 @@ const app = Vue.createApp({
             ]
         };
     },
-    methods: {
-        toggleDetails(){
-            this.detailsAreVisible = !this.detailsAreVisible;
-        }
-    }
 
 });
+
+app.component('friend-contact', {
+    template: `
+    <li :key="friend.id">
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `,
+    data(){
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: "Tom",
+                name: "Tom from Myspace",
+                phone: "505-603-8701",
+                email: "tom@myspace.com"
+            },
+        };
+    },
+    methods: {
+        toggleDetails(){ this.detailsAreVisible = !this.detailsAreVisible; },
+    }
+})
 
 app.mount('#app');
