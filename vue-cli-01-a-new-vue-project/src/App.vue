385d7f46b761bcdc20e693f3f53f3b64e4e5cee2
Theo Weller <weller.theodor@gmail.com>
2021-03-22 17:37:11 -0700

    Vue CLI 1.0 - Created component + added styling

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>My friends!</h2>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            friends: [
+                {
+                    id: "Brennan",
+                    name: "Brennan Kelly",
+                    phone: "(505) 686-3746",
+                    email: "brennan@spins.com"
+                },
+                {
+                    id: "Tom",
+                    name: "Tom Kelly",
+                    phone: "(505) 486-3746",
+                    email: "Tom@spins.com"
+                },
+            ]
+        };
+    }
+};
+</script>
