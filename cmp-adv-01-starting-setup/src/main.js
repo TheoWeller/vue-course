af92f31f1232eed462b05a3cbca53fd866bab795
Theo Weller <weller.theodor@gmail.com>
2021-03-24 09:05:07 -0700

    Advanced Components - 1.2 - SCSS - Scoped styling

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -5,6 +5,7 @@ import BaseBadge from './components/BaseBadge.vue';
 
 const app = createApp(App);
 
+//Components registered globally
 app.component('base-badge', BaseBadge);
 // app.component('badge-list', BadgeList);
 // app.component('user-info', UserInfo);
