8a6925801ddd06e3da0a30b33f36b29def208e1c
Theo Weller <weller.theodor@gmail.com>
2021-03-24 09:12:57 -0700

    Advanced Components - 1.3 - slots introduction

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
+
 
 const app = createApp(App);
 
 //Components registered globally
 app.component('base-badge', BaseBadge);
-// app.component('badge-list', BadgeList);
+app.component('base-card', BaseCard);
 // app.component('user-info', UserInfo);
 
 app.mount('#app');
