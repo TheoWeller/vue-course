98e99b7cac3a26573c80d306c5eaad6aee2bcb07
Theo Weller <weller.theodor@gmail.com>
2021-03-24 17:49:41 -0700

    Component Project - Completion commit

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,86 @@
+<template>
+<base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+        <p>At least one input field is invalid.</p>
+        <p>Please check all input fields.</p>
+    </template>
+    <template #actions>
+        <base-button @click="confirmError">Ok</base-button>
+    </template>
+</base-dialog>
+    <base-card>
+        <form @submit.prevent="submitData">
+            <div class="form-control">
+                <label>Title</label>
+                <input id="title" name="title" type="text" ref="titleInput"/>
+            </div>
+            <div class="form-control">
+                <label>Description</label>
+                <textarea id="description" name="description" type="text" rows="3" ref="descInput"/>
+            </div>
+            <div class="form-control">
+                <label>Link</label>
+                <input id="link" name="link" type="url" ref="linkInput"/>
+            </div>
+            <div>
+                <base-button type="submit">Add Resource</base-button>
+            </div>
+        </form>
+    </base-card>
+</template>
+
+<script>
+import BaseDialog from '../UI/BaseDialog.vue';
+export default {
+  components: { BaseDialog },
+    inject: ['addResource'],
+    data(){
+        return {
+            inputIsInvalid: false,
+        };
+    },
+    methods: {
+        submitData(){
+            const enteredTitle = this.$refs.titleInput.value;
+            const enteredDescription = this.$refs.descInput.value;
+            const enteredLink = this.$refs.linkInput.value;
+
+            if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredLink.trim() === '') {
+                this.inputIsInvalid = true;
+                return;
+            }
+
+            this.addResource(enteredTitle, enteredDescription, enteredLink);
+        },
+        confirmError(){ this.inputIsInvalid = false },
+    },
+}
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
